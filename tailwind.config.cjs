46fb974c7b39e4f3728368ba9dac6a3d2559cd03
maciejg-git <ender@localhost>
2022-11-16 02:04:41 +0100

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -28,6 +28,9 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
+     require("@tailwindcss/forms")({
+    strategy: 'class', // only generate classes
+  }),
+    
   ],
 }
