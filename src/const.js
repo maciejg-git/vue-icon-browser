0a56bac93be5a4dcafa3355917be462e839d1bf9
maciejg-git <m.gralek@gmail.com>
2025-05-28 02:30:18 +0200

    update urls

--- src/const.js
+++ src/const.js
@@ -81,38 +81,42 @@ export const templates = {
 export const urls = {
   B: {
     SVG: "https://raw.githubusercontent.com/twbs/icons/main/icons",
+    normalizedSVG: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-svg-bootstrap",
     download: {
-      vue: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-bootstrap/icons",
+      vue: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-vue-bootstrap/icons",
     },
     github: {
-      vue: "https://github.com/maciejg-git/vue-bootstrap-icons/blob/main/dist-bootstrap/icons",
+      vue: "https://github.com/maciejg-git/vue-bootstrap-icons/blob/main/dist-vue-bootstrap/icons",
     },
   },
   Mdi: {
     SVG: "https://raw.githubusercontent.com/Templarian/MaterialDesign/master/svg",
+    normalizedSVG: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-svg-mdi",
     download: {
-      vue: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-mdi/icons",
+      vue: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-vue-mdi/icons",
     },
     github: {
-      vue: "https://github.com/maciejg-git/vue-bootstrap-icons/blob/main/dist-mdi/icons",
+      vue: "https://github.com/maciejg-git/vue-bootstrap-icons/blob/main/dist-vue-mdi/icons",
     },
   },
   Fa: {
     SVG: "https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs",
+    normalizedSVG: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-svg-fontawesome",
     download: {
-      vue: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-fontawesome/icons",
+      vue: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-vue-fontawesome/icons",
     },
     github: {
-      vue: "https://github.com/maciejg-git/vue-bootstrap-icons/blob/main/dist-fontawesome/icons",
+      vue: "https://github.com/maciejg-git/vue-bootstrap-icons/blob/main/dist-vue-fontawesome/icons",
     },
   },
   H: {
     SVG: "https://raw.githubusercontent.com/tailwindlabs/heroicons/master/optimized",
+    normalizedSVG: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-svg-heroicons",
     download: {
-      vue: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-heroicons/icons",
+      vue: "https://raw.githubusercontent.com/maciejg-git/vue-bootstrap-icons/main/dist-vue-heroicons/icons",
     },
     github: {
-      vue: "https://github.com/maciejg-git/vue-bootstrap-icons/blob/main/dist-heroicons/icons",
+      vue: "https://github.com/maciejg-git/vue-bootstrap-icons/blob/main/dist-vue-heroicons/icons",
     },
   },
 };
